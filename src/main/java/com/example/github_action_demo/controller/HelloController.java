bca6560669b43f49f4611ee539e96966bb74081d
woon <woon7713@gmail.com>
2025-08-26 15:51:44 +0900

    update v2.0

--- src/main/java/com/example/github_action_demo/controller/HelloController.java
+++ src/main/java/com/example/github_action_demo/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.h662.github_action_demo.controller;
+package com.example.github_action_demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
